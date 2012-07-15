071cf59f66702ded4d9a50995f141c51fabb40cd
Aishwarya Singhal <Aishwarya_Singhal@mckinsey.com>
2012-07-15 17:25:21 +0530

    refactored

--- app/helpers/SurveyExporter.scala
+++ app/helpers/SurveyExporter.scala
@@ -22,6 +22,25 @@ import helpers.SurveyHelper._
 object SurveyExporter {
 
   /**
+   * Implicit wrapper to add features to a XSSFRow object.
+   */
+  implicit def RowWrapper(row: org.apache.poi.xssf.usermodel.XSSFRow) = {
+    new {
+      /**
+       * Add a cell to this row and set the given value.
+       *
+       * @param cNum : cell number (position, starts with zero)
+       * @param value to be set
+       **/
+      def +(cNum: Int, value: String) = {
+        val cell = row.createCell(cNum)
+        cell.setCellValue(value)
+        cNum + 1
+      }
+    }
+  }
+
+  /**
    * Exports a surveys responses to Excel.
    *
    * @param id
@@ -33,68 +52,55 @@ object SurveyExporter {
 
     Survey.findOne("surveyId" -> id, "owner" -> user).foreach { s => 
       val qtexts = getQuestionTexts(s.toMap, true)
-      val texts = qtexts._1
+      implicit val texts = qtexts._1
 
       val isOpen = s.get("accessType").toString == "open"
 
-      val wb = new XSSFWorkbook();
-      val fileOut = new FileOutputStream(file);
+      val wb = new XSSFWorkbook
+      val fileOut = new FileOutputStream(file)
       val sheet = wb.createSheet("Responses")
 
       var rNum = 0
       var row = sheet.createRow(rNum.toShort)
 
       var cNum = 0
-      if (!isOpen) {
-        val cell = row.createCell(cNum)
-        cell.setCellValue("Email")
-        cNum += 1
-      }
-      val qIds = qtexts._2.map { case (q, t) =>
-        val cell = row.createCell(cNum)
-        cell.setCellValue(t)
-        cNum += 1
+      if (!isOpen) cNum = row + (cNum, "Email")
+      val qIds = qtexts._2.map { case (q, t) => cNum = row + (cNum, t)
         q
       }
 
-       SurveyResponse.find("surveyId" -> id).foreach { r => 
-         val qResponse = deserialize(classOf[SurveyResponse], r.toMap)
-         rNum += 1
-         cNum = 0
-         row = sheet.createRow(rNum.toShort)
-         if (!isOpen) {
-           val cell = row.createCell(cNum)
-           cell.setCellValue(qResponse.email)
-           cNum += 1
-         }
-         var responses = Map[String, QuestionResponse]()
-         qResponse.responses.foreach { res =>
-           responses += (res.question -> res)
-         }
+      SurveyResponse.find("surveyId" -> id).foreach { r => 
+       val qResponse = deserialize(classOf[SurveyResponse], r.toMap)
+       rNum += 1
+       cNum = 0
+       row = sheet.createRow(rNum.toShort)
+       if (!isOpen) cNum = row + (cNum, qResponse.email)
+       implicit var responses = Map[String, QuestionResponse]()
+       qResponse.responses.foreach { res => responses += (res.question -> res) }
+       qIds.foreach { q => cNum = row + (cNum, getAnswer(q)) }
+      }
 
-         qIds.foreach { q =>
-           val res = responses.getOrElse(q, null)
-           val cell = row.createCell(cNum)
-           var answer: String = null
-           if (res != null)  {
-             val option = if (res.question.indexOf("_") != -1) res.question.split("_")(0) else res.question
-             answer = res.answers.map { a => 
-                 var text = if (res.ranking) (a.replace(res.question + "_", "")) else a
-                 texts.getOrElse(option + "_" + text, text) }.mkString("\n")
-           } else if (q.endsWith("_other")) {
-             val q1 = q.replace("_other", "")
-             val res1 = responses.getOrElse(q1, null)
-             if (res1 != null)  {
-               answer = res1.other
-             }
-           }
-           cell.setCellValue(answer)
-           cNum += 1
-         }
-       }
-
-      wb.write(fileOut);
-      fileOut.close();
+      wb.write(fileOut)
+      fileOut.close
     }
+  }
+
+  private def getAnswer(q: String)(implicit responses: Map[String, QuestionResponse], texts: Map[String, String]) = {
+    val res = responses.getOrElse(q, null)
+    var answer: String = null
+    if (res != null)  {
+      val option = if (res.question.indexOf("_") != -1) res.question.split("_")(0) else res.question
+      answer = res.answers.map { a => 
+         var text = if (res.ranking) (a.replace(res.question + "_", "")) else a
+         texts.getOrElse(option + "_" + text, text) 
+      }.mkString("\n")
+    } else if (q.endsWith("_other")) {
+      val q1 = q.replace("_other", "")
+      val res1 = responses.getOrElse(q1, null)
+      if (res1 != null)  {
+        answer = res1.other
+      }
+    }
+    answer
   }
 }

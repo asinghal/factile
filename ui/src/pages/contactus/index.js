18e8c0a6e5444c00b61075d928c1ee3db1900430
Aishwarya Singhal <asinghal@Aishwaryas-MBP.fritz.box>
2020-06-23 13:23:50 +0200

    further migrations

--- ui/src/pages/contactus/index.js
+++ ui/src/pages/contactus/index.js
@@ -2,6 +2,36 @@ import React from "react";
 
 export default function ContactUs() {
     return (
-        <div />
+        <div class="container">
+          
+        <div class="row">
+          <h1>How Can We Help?</h1>
+          
+          <h3>General Issues</h3>
+          <p>If you want to report a problem, or seek guidance on the tool, or perhaps have a query, please visit <a href="http://groups.google.com/group/factile" target="_blank">our forum</a>.</p>
+          
+          <h3>Bugs/ Feature Requests</h3>
+          <p>If you would like to open a bug/ issue or request a new feature, please raise an issue on <a href="http://github.com/asinghal/factile/issues" target="_blank">github</a>.</p>
+          
+          <h3>Anything Else</h3>
+          <p>For anything else, please checkout our tutorials/ FAQs or drop us a note on <a href="http://groups.google.com/group/factile" target="_blank">our forum</a>, or leave your thoughts below. We will contact you if needed.</p>
+        </div>
+
+        <div class="row"><div class="span12 clearfix">&nbsp;</div></div>
+        <div class="row"><div class="span12 clearfix">&nbsp;</div></div>
+        <div class="row">
+          <div class="span3">&nbsp;</div>
+          <div class="span6">
+            <div id="disqus_thread"></div>
+            <script type="text/javascript">
+                
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+            <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+          </div>
+          <div class="span3">&nbsp;</div>
+        </div>
+
+      </div>
     );
 };

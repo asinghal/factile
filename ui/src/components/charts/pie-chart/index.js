fc6563c6b72ccb8b15ae30ef5553696447f8b401
Aishwarya Singhal <asinghal@Aishwaryas-MBP.fritz.box>
2020-07-11 11:55:49 +0200

    bar charts

--- ui/src/components/charts/pie-chart/index.js
+++ ui/src/components/charts/pie-chart/index.js
@@ -3,13 +3,13 @@ import * as d3 from 'd3';
 
 import './pie-chart.css';
 
-const getArc = () => d3.arc().innerRadius(0).outerRadius(150);
+const getArc = (radius) => d3.arc().innerRadius(0).outerRadius(radius);
 const midAngle = (d) => d.startAngle + (d.endAngle - d.startAngle)/2;
 
 const COLORS = [ '#f69e7b', '#f5b0cb', '#679b9b', '#726a95', '#7d5a5a', '#f5b971', '#e36387', '#889e81', '#5d5b6a', '#bd574e', '#39375b', '#445c3c'];
 
-const Slice = ({ pie }) => {
-    const arc = getArc();
+const Slice = ({ pie, radius }) => {
+    const arc = getArc(radius);
     const interpolate = d3.interpolateRgb('#f4eeff', '#424874');
 
     return pie.map((slice, index) => {
@@ -18,36 +18,36 @@ const Slice = ({ pie }) => {
     });
 };
 
-const Label = ({ pie, labels }) => {
-    const radius = 175;
-    const arc = getArc();
+const Label = ({ pie, labels, radius }) => {
+    const labelRadius = 1.5 * radius;
+    const arc = getArc(radius);
 
     return pie.map((slice, index) => {
         const pos = arc.centroid(slice);
-        pos[0] = radius * (midAngle(slice) < Math.PI ? 1 : -1);
-        pos[1] = 2 * radius * (pos[1]/ 100);
-        return <text key={index} dy="0.35em" textAnchor="middle" transform={`translate(${pos})`} fontSize="0.7em">{labels[index]}</text>;
+        pos[0] = labelRadius * (midAngle(slice) < Math.PI ? 1 : -1);
+        pos[1] = 2 * labelRadius * (pos[1]/ 100);
+        return <text key={index} dy="-2px" textAnchor="start" transform={`translate(${pos})`} fontSize="0.7em">{labels[index]}</text>;
     });
 };
 
-const Line = ({ pie }) => {
-    const radius = 175;
-    const arc = getArc();
+const Line = ({ pie, radius }) => {
+    const labelRadius = 1.5 * radius;
+    const arc = getArc(radius);
 
     return pie.map((slice, index) => {
         const labelPos = arc.centroid(slice);
-		labelPos[0] = radius * 0.9 * (midAngle(slice) < Math.PI ? 1 : -1);
-        labelPos[1] = 2 * radius * (labelPos[1]/ 100);
+		labelPos[0] = labelRadius * 0.9 * (midAngle(slice) < Math.PI ? 1 : -1);
+        labelPos[1] = 2 * labelRadius * (labelPos[1]/ 100);
 
         const outerArcPos = arc.centroid(slice);
-		outerArcPos[0] = radius * 0.5 * (midAngle(slice) < Math.PI ? 1 : -1);
-        outerArcPos[1] = 2 * radius * (outerArcPos[1]/ 100);
+		outerArcPos[0] = labelRadius * 0.5 * (midAngle(slice) < Math.PI ? 1 : -1);
+        outerArcPos[1] = 2 * labelRadius * (outerArcPos[1]/ 100);
 
         return <polyline key={index} points={[arc.centroid(slice), outerArcPos, labelPos]} />;
     });
 };
 
-export default function PieChart({ data }) {
+export default function PieChart({ data, width = 600 }) {
     if (!data || !data.length) {
         return null;
     }
@@ -55,14 +55,14 @@ export default function PieChart({ data }) {
     let pie = d3.pie()(data.map(d => d.value));
 
     const height = 600;
-    const width = 600;
+    const radius= Math.min(width, height)/5;
 
     return (
         <svg height={height} width={width}>
             <g transform={`translate(${width/2}, ${height/2})`}>
-                <Slice pie={pie} />
-                <Label pie={pie} labels={data.map(d => d.name)} />
-                <Line pie={pie} />
+                <Slice pie={pie} radius={radius} />
+                <Label pie={pie} labels={data.map(d => d.name)} radius={radius} />
+                <Line pie={pie} radius={radius} />
             </g>
         </svg>
     );
--- ui/src/pages/survey-responses/index.js
+++ ui/src/pages/survey-responses/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import ReactWordcloud from 'react-wordcloud';
 
@@ -8,12 +8,21 @@ import { findSurvey, getExcelData } from './api.js';
 import './survey-responses.css';
 import PieChart from "../../components/charts/pie-chart/index.js";
 import { writeXlsx } from "./excel.js";
+import BarChart from "../../components/charts/bar-chart/index.js";
 
 
 export default function SurveyResponses() {
     const [surveyResponses, setSurveyResponses] = useState([]);
+    const [chartWidth, setChartWidth] = useState(0);
     const { id } = useParams();
     const history = useHistory();
+
+    const ref = useRef(null);
+
+    useEffect(() => {
+        setChartWidth(ref.current ? (ref.current.offsetWidth * 0.9) : 0);
+    }, [ref.current]);
+
 
     useEffect(() => {
         findSurvey(id).then(setSurveyResponses).catch(() => history.replace('/'));
@@ -30,7 +39,7 @@ export default function SurveyResponses() {
                 <div className="col-md-3 col-sm-12">
                     <SurveyManagementMenu surveyId={id} />
                 </div>
-                <div className="col-md-9 col-sm-12">
+                <div ref={ref} className="col-md-9 col-sm-12">
                     <div>
                         <button onClick={Download} className="base-btn submit-btn">Download in Excel</button>
                     </div>
@@ -39,7 +48,7 @@ export default function SurveyResponses() {
                             <div className="block" key={surveyResponse.question}>
                                 <div><strong>{surveyResponse.texts.question}</strong></div>
                                 {!surveyResponse.hasOptions &&
-                                <div style={{ height: 400, width: 600 }}>
+                                <div style={{ height: 400, width: chartWidth }}>
                                     <ReactWordcloud words={surveyResponse.words} />
                                 </div>
                                 }
@@ -64,7 +73,12 @@ export default function SurveyResponses() {
                                             ))}
                                         </tbody>
                                     </table>
-                                    <PieChart data={surveyResponse.answers} key={`${surveyResponse.question}-pie`} />
+                                    {surveyResponse.answers && surveyResponse.answers.length < 6 &&
+                                        <PieChart data={surveyResponse.answers} key={`${surveyResponse.question}-pie`} width={chartWidth} />
+                                    }
+                                    {surveyResponse.answers && surveyResponse.answers.length >= 6 &&
+                                        <BarChart data={surveyResponse.answers} key={`${surveyResponse.question}-bar`} width={chartWidth} />
+                                    }
                                 </div>
                                 }
                             </div>

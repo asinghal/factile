894d9b26647f23c863b08f32e7da486385b046a2
Aishwarya Singhal <asinghal@Aishwaryas-MBP.fritz.box>
2020-07-13 18:13:49 +0200

    refactor survey response display

--- ui/src/pages/survey-responses/index.js
+++ ui/src/pages/survey-responses/index.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useParams, useHistory } from "react-router-dom";
-import ReactWordcloud from 'react-wordcloud';
 
 import SurveyManagementMenu from "../../components/surveys/survey-management-menu/index.js";
 
 import { findSurvey, getExcelData } from './api.js';
 import './survey-responses.css';
-import PieChart from "../../components/charts/pie-chart/index.js";
 import { writeXlsx } from "./excel.js";
-import BarChart from "../../components/charts/bar-chart/index.js";
+import SurveyResponseBlock from "./response-block.js";
 
 
 export default function SurveyResponses() {
@@ -45,43 +43,7 @@ export default function SurveyResponses() {
                     </div>
                     <div>
                         {surveyResponses && surveyResponses.map(surveyResponse => (
-                            <div className="block" key={surveyResponse.question}>
-                                <div><strong>{surveyResponse.texts.question}</strong></div>
-                                {!surveyResponse.hasOptions &&
-                                <div style={{ height: 400, width: chartWidth }}>
-                                    <ReactWordcloud words={surveyResponse.words} />
-                                </div>
-                                }
-                                {!surveyResponse.hasOptions && surveyResponse.answers.map((answer, index) => (
-                                    <div className="answer" key={surveyResponse.question + "-" + index}>{answer}</div>
-                                ))}
-                                {surveyResponse.hasOptions && 
-                                <div>
-                                    <table className="table">
-                                        <thead>
-                                            <tr>
-                                                <th>Option</th>
-                                                <th>Count</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {surveyResponse.answers.map((answer, index) => (
-                                            <tr key={surveyResponse.question + "-" + index}>
-                                                <td>{answer.name}</td>
-                                                <td>{answer.value}</td>
-                                            </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-                                    {surveyResponse.answers && surveyResponse.answers.length < 6 &&
-                                        <PieChart data={surveyResponse.answers} key={`${surveyResponse.question}-pie`} width={chartWidth} />
-                                    }
-                                    {surveyResponse.answers && surveyResponse.answers.length >= 6 &&
-                                        <BarChart data={surveyResponse.answers} key={`${surveyResponse.question}-bar`} width={chartWidth} />
-                                    }
-                                </div>
-                                }
-                            </div>
+                            <SurveyResponseBlock surveyResponse={surveyResponse} chartWidth={chartWidth} key={surveyResponse.question} />
                         ))}
                     </div>
                 </div>
---
+++ ui/src/pages/survey-responses/response-block.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import ReactWordcloud from 'react-wordcloud';
+
+import PieChart from "../../components/charts/pie-chart/index.js";
+import BarChart from "../../components/charts/bar-chart/index.js";
+
+export default function SurveyResponseBlock({ surveyResponse, chartWidth }) {
+
+    const [collapsed, setCollapsed] = useState(true);
+
+    return (
+        <div className="block">
+            <div className="question-header">
+                <div>
+                    <strong>{surveyResponse.texts.question}</strong>
+                </div>
+                <div>
+                    {!collapsed &&
+                        <i className="icon fas fa-chevron-circle-up" onClick={() => setCollapsed(!collapsed)}></i>
+                    }
+                    {collapsed &&
+                        <i className="icon fas fa-chevron-circle-down" onClick={() => setCollapsed(!collapsed)}></i>
+                    }
+                </div>
+            </div>
+            {!collapsed &&
+            <>
+                {!surveyResponse.hasOptions &&
+                <div style={{ height: 400, width: chartWidth }}>
+                    <ReactWordcloud words={surveyResponse.words} />
+                </div>
+                }
+                {!surveyResponse.hasOptions && surveyResponse.answers.map((answer, index) => (
+                    <div className="answer" key={surveyResponse.question + "-" + index}>{answer}</div>
+                ))}
+                {surveyResponse.hasOptions && 
+                <div>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Option</th>
+                                <th>Count</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {surveyResponse.answers.map((answer, index) => (
+                            <tr key={surveyResponse.question + "-" + index}>
+                                <td>{answer.name}</td>
+                                <td>{answer.value}</td>
+                            </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    {surveyResponse.answers && surveyResponse.answers.length < 6 &&
+                        <PieChart data={surveyResponse.answers} key={`${surveyResponse.question}-pie`} width={chartWidth} />
+                    }
+                    {surveyResponse.answers && surveyResponse.answers.length >= 6 &&
+                        <BarChart data={surveyResponse.answers} key={`${surveyResponse.question}-bar`} width={chartWidth} />
+                    }
+                </div>
+                }
+            </>
+            }
+        </div>
+    );
+};

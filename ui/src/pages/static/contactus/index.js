d46c9007fba337b3e3f910139a5066702169c969
Aishwarya Singhal <asinghal79@gmail.com>
2020-07-16 17:06:43 +0200

    added Disqus

--- ui/src/pages/static/contactus/index.js
+++ ui/src/pages/static/contactus/index.js
@@ -23,11 +23,25 @@ export default function ContactUs() {
           <div className="span3">&nbsp;</div>
           <div className="span6">
             <div id="disqus_thread"></div>
-            <script type="text/javascript">
-                
-            </script>
-            <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-            <a href="http://disqus.com" className="dsq-brlink">comments powered by <span className="logo-disqus">Disqus</span></a>
+            <script>
+              {/**
+              *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+              *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+              /*
+              var disqus_config = function () {
+              this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+              this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+              };
+              */
+              (function() { // DON'T EDIT BELOW THIS LINE
+              var d = document, s = d.createElement('script');
+              s.src = 'https://factile.disqus.com/embed.js';
+              s.setAttribute('data-timestamp', +new Date());
+              (d.head || d.body).appendChild(s);
+              })()
+            }
+              </script>
+              <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
           </div>
           <div className="span3">&nbsp;</div>
         </div>

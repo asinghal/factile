1e71351fd581d98450ad0de5317bffa6a7118d35
Aishwarya Singhal <asinghal79@gmail.com>
2020-07-16 15:59:24 +0200

    Circleci project setup (#30)

--- api/tests/db.test.js
+++ api/tests/db.test.js
@@ -5,47 +5,59 @@ const db = require('../lib/db');
 
 describe('db tests', () => {
 
+    let methodStub, clientConnectStub, clientDbStub;
+
+    beforeEach(() => {
+        clientConnectStub = sinon.stub(mongodb.MongoClient.prototype, 'connect').yields(null);
+        clientDbStub = sinon.stub(mongodb.MongoClient.prototype, 'db').returns({collection: (name) => mongodb.Collection.prototype});
+    });
+
+    afterEach(() => {
+        if (methodStub) {
+            methodStub.restore();
+        }
+
+        clientConnectStub.restore();
+        clientDbStub.restore();
+    });
+
     test('list', done => {
-        const findStub = sinon.stub(mongodb.Collection.prototype, 'find').returns({
+        methodStub = sinon.stub(mongodb.Collection.prototype, 'find').returns({
             toArray: sinon.stub().yields(null, [{a: 1}])
         });
 
         db.list('somecollection').then(d => {
             expect(d.length).toBe(1);
-            findStub.restore();
             done();
         });
     });
 
     test('find', done => {
-        const findStub = sinon.stub(mongodb.Collection.prototype, 'find').returns({
+        methodStub = sinon.stub(mongodb.Collection.prototype, 'find').returns({
             toArray: sinon.stub().yields(null, [{a: 1}])
         });
 
         db.find('somecollection').then(d => {
             expect(d.length).toBe(1);
-            findStub.restore();
             done();
         });
     });
 
     test('findOne', done => {
-        const findStub = sinon.stub(mongodb.Collection.prototype, 'findOne').yields(null, {a: 1});
+        methodStub = sinon.stub(mongodb.Collection.prototype, 'findOne').yields(null, {a: 1});
 
         db.findOne('somecollection').then(d => {
             expect(d).not.toBeNull();
             expect(d.a).toBe(1);
-            findStub.restore();
             done();
         });
     });
 
     test('save', done => {
-        const replaceStub = sinon.stub(mongodb.Collection.prototype, 'replaceOne').resolves({});
+        methodStub = sinon.stub(mongodb.Collection.prototype, 'replaceOne').resolves({});
 
         db.save('somecollection', {a: 1, b: 2}, 'a').then(d => {
             expect(d).not.toBeNull();
-            replaceStub.restore();
             done();
         });
     });

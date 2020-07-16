bdc105ca403d9bd91c7be4c4335131b57b97467c
Aishwarya Singhal <asinghal79@gmail.com>
2020-07-16 13:58:57 +0200

    isolating method stubs in test

--- api/tests/db.test.js
+++ api/tests/db.test.js
@@ -5,47 +5,51 @@ const db = require('../lib/db');
 
 describe('db tests', () => {
 
+    let methodStub;
+
+    afterEach(() => {
+        if (methodStub) {
+            methodStub.restore();
+        }
+    });
+
     test('list', done => {
-        const findStub = sinon.stub(mongodb.Collection.prototype, 'find').returns({
+        methodStub = sinon.stub(mongodb.Collection.prototype, 'find').returns({
             toArray: sinon.stub().yields(null, [{a: 1}])
         });
 
         db.list('somecollection').then(d => {
             expect(d.length).toBe(1);
-            findStub.restore();
             done();
         });
     });
 
     test('find', done => {
-        const findStub = sinon.stub(mongodb.Collection.prototype, 'find').returns({
+        methodStub = sinon.stub(mongodb.Collection.prototype, 'find').returns({
             toArray: sinon.stub().yields(null, [{a: 1}])
         });
 
         db.find('somecollection').then(d => {
             expect(d.length).toBe(1);
-            findStub.restore();
             done();
         });
     });
 
     test('findOne', done => {
-        const findStub = sinon.stub(mongodb.Collection.prototype, 'findOne').yields(null, {a: 1});
+        methodStub = sinon.stub(mongodb.Collection.prototype, 'findOne').yields(null, {a: 1});
 
         db.findOne('somecollection').then(d => {
             expect(d).not.toBeNull();
             expect(d.a).toBe(1);
-            findStub.restore();
             done();
         });
     });
 
     test('save', done => {
-        const replaceStub = sinon.stub(mongodb.Collection.prototype, 'replaceOne').resolves({});
+        methodStub = sinon.stub(mongodb.Collection.prototype, 'replaceOne').resolves({});
 
         db.save('somecollection', {a: 1, b: 2}, 'a').then(d => {
             expect(d).not.toBeNull();
-            replaceStub.restore();
             done();
         });
     });

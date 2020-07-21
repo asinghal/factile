8bfcbe27306c293e3a7981c0d0bd7f0bd745ce86
Aishwarya Singhal <asinghal79@gmail.com>
2020-07-21 13:47:48 +0200

    fixed contact us page css

--- ui/src/pages/static/contactus/index.js
+++ ui/src/pages/static/contactus/index.js
@@ -1,27 +1,42 @@
 import React from "react";
 
+import './contact-us.css';
+
 export default function ContactUs() {
     return (
-        <div className="container">
+        <div className="container contact-us">
           
         <div className="row">
-          <h1>How Can We Help?</h1>
-          
-          <h3>General Issues</h3>
-          <p>If you want to report a problem, or seek guidance on the tool, or perhaps have a query, please visit <a href="http://groups.google.com/group/factile" target="_blank" rel="noopener noreferrer">our forum</a>.</p>
-          
-          <h3>Bugs/ Feature Requests</h3>
-          <p>If you would like to open a bug/ issue or request a new feature, please raise an issue on <a href="http://github.com/asinghal/factile/issues" target="_blank" rel="noopener noreferrer">github</a>.</p>
-          
-          <h3>Anything Else</h3>
-          <p>For anything else, please checkout our tutorials/ FAQs or drop us a note on <a href="http://groups.google.com/group/factile" target="_blank" rel="noopener noreferrer">our forum</a>, or leave your thoughts below. We will contact you if needed.</p>
+          <div className="col-12">
+            <h1>How Can We Help?</h1>
+          </div>
         </div>
 
-        <div className="row"><div className="span12 clearfix">&nbsp;</div></div>
-        <div className="row"><div className="span12 clearfix">&nbsp;</div></div>
         <div className="row">
-          <div className="span3">&nbsp;</div>
-          <div className="span6">
+          <div className="col-12">
+            <h4><i className="fas fa-chevron-circle-right blue" aria-hidden="true"></i>&nbsp;General Issues</h4>
+            <p>If you want to report a problem, or seek guidance on the tool, or perhaps have a query, please visit <a href="http://groups.google.com/group/factile" target="_blank" rel="noopener noreferrer">our forum</a>.</p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-12">
+            <h4><i className="fas fa-chevron-circle-right blue" aria-hidden="true"></i>&nbsp;Bugs/ Feature Requests</h4>
+            <p>If you would like to open a bug/ issue or request a new feature, please raise an issue on <a href="http://github.com/asinghal/factile/issues" target="_blank" rel="noopener noreferrer">github</a>.</p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-12">
+            <h4><i className="fas fa-chevron-circle-right blue" aria-hidden="true"></i>&nbsp;Anything Else</h4>
+            <p>For anything else, please checkout our tutorials/ FAQs or drop us a note on <a href="http://groups.google.com/group/factile" target="_blank" rel="noopener noreferrer">our forum</a>, or leave your thoughts below. We will contact you if needed.</p>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="row">
+          <div className="col-12">
             <div id="disqus_thread"></div>
             <script>
               {/**
@@ -43,7 +58,6 @@ export default function ContactUs() {
               </script>
               <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
           </div>
-          <div className="span3">&nbsp;</div>
         </div>
 
       </div>

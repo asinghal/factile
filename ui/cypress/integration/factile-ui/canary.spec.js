18c4825598e9cad44fc797a0cb8dd59c17326dfa
Aishwarya Singhal <asinghal79@gmail.com>
2020-07-29 15:00:03 +0200

    back to hit and trial with cypress

--- ui/cypress/integration/factile-ui/canary.spec.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-describe('Canary Test', () => {
-    it('should ensure that the set up is working', () => {
-      cy.visit('/');
-      cy.get('[data-testid="login-btn"]').should('have.text', 'Login');
-    });
-});

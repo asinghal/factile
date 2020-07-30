cf16502fa85935a895289d4d5bd85b66a7368209
Aishwarya Singhal <asinghal79@gmail.com>
2020-07-30 08:33:23 +0200

    check for all question types

--- ui/cypress/integration/factile-ui/newsurvey.spec.js
+++ ui/cypress/integration/factile-ui/newsurvey.spec.js
@@ -60,7 +60,7 @@ describe('New Survey Test', () => {
     });
 
     describe('Questionnaire', () => {
-        it('should successfully add questions to a survey', () => {
+        beforeEach(() => {
             cy.route({
                 url: '/api/surveys/abcd-xyz-123456',
                 method: 'GET',
@@ -76,7 +76,9 @@ describe('New Survey Test', () => {
             cy.visit('/surveys/abcd-xyz-123456/questions');
             cy.wait('@thisSurvey');
             cy.get('[data-testid="survey-title"]').should('have.text', 'Test survey');
+        });
 
+        it('should successfully add a radio button question to a survey', () => {
             // click the add questions button
             cy.get('[data-testid="btn-add-questions-0"]').click();
             // select "single choice" question
@@ -88,7 +90,145 @@ describe('New Survey Test', () => {
             cy.get('#label-q1-mandatory').click();
             cy.get('[data-testid="btn-save-details-0"]').click();
             cy.wait('@updatethisSurvey');
-         });
+        });
+
+        it('should successfully add a dropdown question to a survey', () => {
+            // click the add questions button
+            cy.get('[data-testid="btn-add-questions-0"]').click();
+            // select "single choice" question
+            cy.get('[data-testid="add-questions-0-option-dropdown"]').click();
+
+            cy.get('[data-testid="question-block-q1"] h4').should('have.text', 'Dropdown');
+            cy.get('[data-testid="question-block-q1"] input[name="text"]').type('Howzat?', { delay: 100 });
+            cy.get('[data-testid="question-block-q1"] textarea[name="options"]').type('Out\nNot Out\nRefer', { delay: 100 });
+            cy.get('#label-q1-mandatory').click();
+            cy.get('[data-testid="btn-save-details-0"]').click();
+            cy.wait('@updatethisSurvey');
+        });
+
+        it('should successfully add a checkbox question to a survey', () => {
+            // click the add questions button
+            cy.get('[data-testid="btn-add-questions-0"]').click();
+            // select "single choice" question
+            cy.get('[data-testid="add-questions-0-option-checkbox"]').click();
+
+            cy.get('[data-testid="question-block-q1"] h4').should('have.text', 'Multiple Choice');
+            cy.get('[data-testid="question-block-q1"] input[name="text"]').type('Howzat?', { delay: 100 });
+            cy.get('[data-testid="question-block-q1"] textarea[name="options"]').type('Out\nNot Out\nRefer', { delay: 100 });
+            cy.get('#label-q1-mandatory').click();
+            cy.get('[data-testid="btn-save-details-0"]').click();
+            cy.wait('@updatethisSurvey');
+        });
+
+        it('should successfully add a ranking question to a survey', () => {
+            // click the add questions button
+            cy.get('[data-testid="btn-add-questions-0"]').click();
+            // select "single choice" question
+            cy.get('[data-testid="add-questions-0-option-ranking"]').click();
+
+            cy.get('[data-testid="question-block-q1"] h4').should('have.text', 'Ranking');
+            cy.get('[data-testid="question-block-q1"] input[name="text"]').type('Howzat?', { delay: 100 });
+            cy.get('[data-testid="question-block-q1"] textarea[name="options"]').type('Out\nNot Out\nRefer', { delay: 100 });
+            cy.get('#label-q1-mandatory').click();
+            cy.get('[data-testid="btn-save-details-0"]').click();
+            cy.wait('@updatethisSurvey');
+        });
+
+        it('should successfully add a rating question to a survey', () => {
+            // click the add questions button
+            cy.get('[data-testid="btn-add-questions-0"]').click();
+            // select "single choice" question
+            cy.get('[data-testid="add-questions-0-option-rating"]').click();
+
+            cy.get('[data-testid="question-block-q1"] h4').should('have.text', 'Rating Scale');
+            cy.get('[data-testid="question-block-q1"] input[name="text"]').type('Umpiring', { delay: 100 });
+            cy.get('[data-testid="question-block-q1"] textarea[name="options"]').type('Out\nNot Out\nRefer', { delay: 100 });
+            cy.get('[data-testid="question-block-q1"] textarea[name="dimensions"]').type('Howzat\nWhat do you say?', { delay: 100 });
+            cy.get('#label-q1-mandatory').click();
+            cy.get('[data-testid="btn-save-details-0"]').click();
+            cy.wait('@updatethisSurvey');
+        });
+
+        it('should successfully add a textbox question to a survey', () => {
+            // click the add questions button
+            cy.get('[data-testid="btn-add-questions-0"]').click();
+            // select "single choice" question
+            cy.get('[data-testid="add-questions-0-option-textbox"]').click();
+
+            cy.get('[data-testid="question-block-q1"] h4').should('have.text', 'Text Field');
+            cy.get('[data-testid="question-block-q1"] input[name="text"]').type('Howzat?', { delay: 100 });
+            cy.get('#label-q1-mandatory').click();
+            cy.get('[data-testid="btn-save-details-0"]').click();
+            cy.wait('@updatethisSurvey');
+        });
+
+        it('should successfully add a textarea question to a survey', () => {
+            // click the add questions button
+            cy.get('[data-testid="btn-add-questions-0"]').click();
+            // select "single choice" question
+            cy.get('[data-testid="add-questions-0-option-textarea"]').click();
+
+            cy.get('[data-testid="question-block-q1"] h4').should('have.text', 'Large Text/ Essay');
+            cy.get('[data-testid="question-block-q1"] input[name="text"]').type('Howzat?', { delay: 100 });
+            cy.get('#label-q1-mandatory').click();
+            cy.get('[data-testid="btn-save-details-0"]').click();
+            cy.wait('@updatethisSurvey');
+        });
+
+        it('should successfully add a plain text question to a survey', () => {
+            // click the add questions button
+            cy.get('[data-testid="btn-add-questions-0"]').click();
+            // select "single choice" question
+            cy.get('[data-testid="add-questions-0-option-plaintext"]').click();
+
+            cy.get('[data-testid="question-block-q1"] h4').should('have.text', 'Plain Text');
+            cy.get('[data-testid="question-block-q1"] input[name="text"]').type('Howzat?', { delay: 100 });
+            cy.get('[data-testid="btn-save-details-0"]').click();
+            cy.wait('@updatethisSurvey');
+        });
+
+        it('should successfully add multiple questions to a survey', () => {
+            // click the add questions button
+            cy.get('[data-testid="btn-add-questions-0"]').click();
+            // select "single choice" question
+            cy.get('[data-testid="add-questions-0-option-plaintext"]').click();
+
+            cy.get('[data-testid="question-block-q1"] h4').should('have.text', 'Plain Text');
+            cy.get('[data-testid="question-block-q1"] input[name="text"]').type('Howzat?', { delay: 100 });
+
+            // click the add questions button
+            cy.get('[data-testid="btn-add-questions-0"]').click();
+            // select "single choice" question
+            cy.get('[data-testid="add-questions-0-option-page"]').click();
+
+            // click the add questions button
+            cy.get('[data-testid="btn-add-questions-0"]').click();
+            // select "single choice" question
+            cy.get('[data-testid="add-questions-0-option-textarea"]').click();
+
+            cy.get('[data-testid="question-block-q3"] h4').should('have.text', 'Large Text/ Essay');
+            cy.get('[data-testid="question-block-q3"] input[name="text"]').type('Howzat?', { delay: 100 });
+            cy.get('#label-q3-mandatory').click();
+
+            cy.get('[data-testid="btn-save-details-0"]').click();
+            cy.wait('@updatethisSurvey');
+        });
+
+        it('should delete question', () => {
+            // click the add questions button
+            cy.get('[data-testid="btn-add-questions-0"]').click();
+            // select "single choice" question
+            cy.get('[data-testid="add-questions-0-option-plaintext"]').click();
+
+            cy.get('[data-testid="question-block-q1"] h4').should('have.text', 'Plain Text');
+            cy.get('[data-testid="question-block-q1"] input[name="text"]').type('Howzat?', { delay: 100 });
+
+            cy.get('[data-testid="question-block-q1"] .delete-btn').click();
+
+            cy.get('[data-testid="question-block-q1"]').should('not.exist');
+            // delete will call update as well
+            cy.wait('@updatethisSurvey');
+        });
     });
 
 });
